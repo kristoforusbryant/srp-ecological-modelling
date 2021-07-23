1cb0b1b937643d5300d6f60c8d504a7309a69ae7
kristoforusbryant <kristoforusbryant@gmail.com>
2021-07-23 10:24:54 +0800

    edited tests

--- tests/test_Landscape.py
+++ tests/test_Landscape.py
@@ -1,10 +1,4 @@
 import numpy as np
-import sys, os
-
-currdir = os.path.dirname(__file__)
-sys.path.append(os.path.dirname(currdir))
-print(os.path.dirname(currdir))
-print(sys.path)
 
 # Parser
 import Parser
@@ -13,69 +7,52 @@ def test_parser():
     """
     sys_args = ['--outfile', 'results/out_matrix.pkl', '--params', 'params.json']
     parser = Parser.Parser(sys_args)
-    assert(isinstance(parser.args.outfile, str))
-    assert(isinstance(parser.args.params, str))
+    assert(isinstance(parser.args.file, str))
+    assert(isinstance(parser.args.dollar_size, str))
 
 # Landscape: LSP
-# test_sample_0 and test_sample_1 does not change from task-01
 from Landscape import LSP
 def test_sample_0():
-    lsp = LSP([1,2,3,4], [10, 20, 30, 40, 40], [0, 10, 20, 10, 30], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1])
+    lsp = LSP([1,2,3,4], [10, 20, 30, 40, 40], [0, 10, 20, 10, 30])
     for _ in np.arange(100):
         assert(lsp.sample() in [1,2,3,4])
 
 def test_sample_1():
     for _ in np.arange(50):
         l = np.random.choice(np.arange(100), 5)
-        lsp = LSP(l, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1])
+        lsp = LSP(l, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30])
         for _ in np.arange(100):
             assert(lsp.sample() in l)
 
-def test_sample_2():
-    lsp = LSP([1,2,3,4], [], [], recr_prob=[0, 0, 1, 0], mort_prob=[1, 1, 1, 1])
-    for _ in np.arange(100):
-        assert(lsp.sample() == 3)
-
 def test_simulate_0():
-    lsp = LSP([1,2,3,4], [10, 20, 30, 40, 40], [0, 10, 20, 10, 30],
-              recr_prob=[0, 0, 1, 0], mort_prob=[1,1,1,1])
-    trajectory = np.array([10, 20, 30, 40, 40]) - np.array([0, 10, 20, 10, 30])
+    lsp = LSP([1,2,3,4], [10, 20, 30, 40, 40], [0, 10, 20, 10, 30])
+    trajectory = np.cumsum(np.array(lsp.recr_trajectory) - \
+                           np.array(lsp.mort_trajectory))
     M = lsp.simulate()
     assert(M.shape == (4, 5))
-    assert(M.sum(axis=0) == trajectory)
-    assert((M[2] > 0).all())
-    for i in [0,1,3]:
-        assert((M[i] == 0).all())
+    assert((M.sum(axis=0) == trajectory).all())
 
-def test_simulate_1():
-    raise NotImplementedError
+# def test_simulate_1():
 
 # Landscape: GSP
 from Landscape import GSP
 def test_initialise_LSPs():
     gsp = GSP(np.arange(10))
-    gsp.initialise_LSPs(4, 5, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30],
-                        [1, 1, 1, 1], [1, 1, 1, 1])
+    gsp.initialise_LSPs(4, 5, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30])
+
     assert(len(gsp.LSP_list) == 4)
     for i in np.arange(4):
         assert(gsp.LSP_list[i].get_size() == 5)
 
-def test_simulate_GSP_0():
+def test_simulate():
     gsp = GSP(np.arange(10))
-    gsp.initialise_LSPs(4, 5, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30],
-                        recr_prob=[0, 0, 1, 0], mort_prob=[1, 1, 1, 1])
-    trajectory = np.array([10, 20, 30, 40, 40]) - np.array([0, 10, 20, 10, 30])
+    gsp.initialise_LSPs(4, 10, [10, 20, 30, 40, 40], [0, 10, 20, 10, 30])
+    trajectory = np.cumsum(np.array(gsp.LSP_list[0].recr_trajectory) \
+                           - np.array(gsp.LSP_list[0].mort_trajectory))
     M = gsp.simulate()
 
-    assert(M.shape == (4, 5, 5))
-    for i in range(4):
+    assert(M.shape == (4, 10, 5))
+    for i in np.arange(4):
         assert((M[i].sum(axis=0) == trajectory).all())
-        assert((M[i, 2, :] > 0).all()) # recruit with probability 1 -> species is always in the landscape
-        for j in [0, 1, 3]:
-            assert((M[i, j, :] == 0).all()) # recruit with probability 0 -> species is never in the landscape
-
-def test_simulate_GSP_1():
-    raise NotImplementedError
-
 
 
